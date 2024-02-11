e2314c33f20382792f60a3dc8b3a0d1607229e1e
Henrique Lindozo <138804485+devr1q@users.noreply.github.com>
2024-02-11 03:31:38 -0300

    to aprendo

--- curso-completo/fundamentos.js
+++ curso-completo/fundamentos.js
@@ -127,3 +127,82 @@ do {
 } while (j < 5);
 
 // Funções
+// function nome(arg1, arg2) { corpo }
+
+function cumprimentar(nome) {
+    console.log("Olá, " + nome + "!")
+}
+
+// invocação = nome()
+cumprimentar("Ana Julia");
+
+// escopo de variaveis
+let cor = "azul marinho";
+
+function mostrarCor() {
+    let cor = "verde";
+
+    console.log(cor);
+}
+
+console.log(cor);
+mostrarCor();
+
+// hoisting = içamento
+testeHoisting();
+
+function testeHoisting() {
+    console.log("Deu certo!");
+}
+
+// arrow function
+const testeArrow = () => console.log("Isso tambpem é uma função!");
+
+testeArrow();
+
+
+// truthy e falsy
+const minhaVariavel1 = ""; // falsy
+const minhaVariavel2 = "Algum texto"; // truthy
+
+if (minhaVariavel1) {
+    console.log("É verdadeiro!");
+} else {
+    console.log("É falso!");
+}
+
+if (minhaVariavel2) {
+    console.log("É verdadeiro! 2");
+} else {
+    console.log("É falso! 2");
+}
+
+// array, listas => começando sempre a contar pelo 0
+const numeros = [1, 2, 3, 4, 5];
+
+console.log(numeros);
+
+console.log(numeros[1]);
+console.log(numeros[3]);
+
+// adiciona um elemento no array = .push
+
+numeros.push(6);
+console.log(numeros);
+
+
+// elimina um elemento no array = .pop
+
+numeros.pop();
+console.log(numeros);
+
+// strings
+const minhaStringNova = "Olá, String!";
+
+const minhaString3 = minhaStringNova + " Como você está?"
+console.log(minhaString3);
+
+// interpolação
+
+const minhaString4 = `${minhaStringNova} Como nós está?`;
+console.log(minhaString4);
