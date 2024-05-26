35b9beefb8978289ffddba22792b59ae38759dc7
Henrique Lindozo <138804485+devr1q@users.noreply.github.com>
2024-05-26 15:02:46 -0300

    to criando

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,10 @@
-alert("olá");
+//alert("olá");
+
+function carregar() {
+    let msg = window.document.getElementById('msg');
+    let img = window.document.getElementById('alt-imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    msg.innerHTML = `Agora são ${hora} horas.`;
+}
+
