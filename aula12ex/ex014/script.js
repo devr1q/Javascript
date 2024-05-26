a6e0d9f415857109d9f66f35fa982f2feb0f92b9
Henrique Lindozo <138804485+devr1q@users.noreply.github.com>
2024-05-26 17:11:33 -0300

    consertando

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -4,7 +4,17 @@ function carregar() {
     let msg = window.document.getElementById('msg');
     let img = window.document.getElementById('alt-imagem');
     var data = new Date();
-    var hora = data.getHours();
+    // var hora = data.getHours();
+    var hora = 13
     msg.innerHTML = `Agora são ${hora} horas.`;
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = 'fotomanha.jpg'
+    } else if (hora >= 12 && hora < 18) {
+        //boa tarde
+        img.src = 'fototarde.jpg'
+    } else {
+        //boa noite
+        img.src = 'fotonoite.jpg'
+    }
 }
-
